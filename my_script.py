d66b1cb4344b8938d3649cdb96934027ef0116d9
Shantanu Dash <shantanudash7@gmail.com>
2021-06-11 23:22:57 +0530

    Update my_script.py

--- my_script.py
+++ my_script.py
@@ -4,7 +4,14 @@ from pdf2image import convert_from_path
 
 # Store Pdf with convert_from_path function
 images = convert_from_path('main.pdf')
-images[0].save('main.png','PNG')
+def get_concat_v(im1, im2):
+    dst = Image.new('RGB', (im1.width, im1.height + im2.height))
+    dst.paste(im1, (0, 0))
+    dst.paste(im2, (0, im1.height))
+    return dst
+get_concat_v(images[0],images[1]).save('main.png','PNG')
+
+#images[0].save('main.png','PNG')
 '''
 for i in range(len(images)):
 
